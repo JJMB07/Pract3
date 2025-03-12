6d7f0bd714034496f779c809a7c8e1173a697e77
JJMB07 <2006jjmb@gmail.com>
2025-03-12 23:26:55 +0100

    Add spanish date

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -61,10 +61,10 @@ public class Date {
    *     to pass).
    */
 //Constructor modified by the collaborator
-public Date(int tag, int monat, int jahr) {
-this.day = tag;
-this.month = monat;
-this.year = jahr;
+  public Date(int dia, int mes, int año) {
+    this.day = dia;
+    this.month = mes;
+    this.year = año;
 }
 
   /**
